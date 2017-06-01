cdf5b43c7950ac7b58cff6346fd624574c2a9572
Tobias Wallura <tobias@wallura.org>
2017-06-01 22:11:18 +0200

    disable jre7 in test, as the server jre 7 is not available for download

--- test/integration/default/serverspec/java_home_spec.rb
+++ test/integration/default/serverspec/java_home_spec.rb
@@ -6,11 +6,6 @@ describe command("echo $JAVA_HOME") do
   its(:stdout) { should match '/opt/oracle/jre8' }
 end
 
-describe file('/opt/oracle/jre7') do
-  it { should be_symlink }
-  it { should exist }
-end
-
 describe file('/opt/oracle/jre8') do
   it { should be_symlink }
   it { should exist }
