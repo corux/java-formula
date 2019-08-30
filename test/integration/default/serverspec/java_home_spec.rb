09f46a486af7ed6baa72549d3ae9a6c2b773419d
Tobias Wallura <tobias@wallura.org>
2019-08-30 22:52:38 +0200

    include jre 12.0.2

--- test/integration/default/serverspec/java_home_spec.rb
+++ test/integration/default/serverspec/java_home_spec.rb
@@ -15,3 +15,8 @@ describe file('/opt/oracle/jre11') do
   it { should be_symlink }
   it { should exist }
 end
+
+describe file('/opt/oracle/jre12') do
+  it { should be_symlink }
+  it { should exist }
+end
