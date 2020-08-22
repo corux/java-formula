89337380885e08aef9e4369523610216514f1b5e
Tobias Wallura <tobias@wallura.org>
2020-08-22 07:37:27 +0200

    remove support for outdated JRE 12 and 13

--- test/integration/default/serverspec/java_home_spec.rb
+++ test/integration/default/serverspec/java_home_spec.rb
@@ -16,16 +16,6 @@ describe file('/opt/oracle/jre11') do
   it { should exist }
 end
 
-describe file('/opt/oracle/jre12') do
-  it { should be_symlink }
-  it { should exist }
-end
-
-describe file('/opt/oracle/jre13') do
-  it { should be_symlink }
-  it { should exist }
-end
-
 describe file('/opt/oracle/jre14') do
   it { should be_symlink }
   it { should exist }
