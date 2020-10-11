bfcaa9e93436a8fbf615a32b0df86fccd25926cb
Tobias Wallura <tobias@wallura.org>
2020-10-11 19:30:15 +0200

    update to JRE 15

--- test/integration/default/serverspec/java_home_spec.rb
+++ test/integration/default/serverspec/java_home_spec.rb
@@ -16,7 +16,7 @@ describe file('/opt/oracle/jre11') do
   it { should exist }
 end
 
-describe file('/opt/oracle/jre14') do
+describe file('/opt/oracle/jre15') do
   it { should be_symlink }
   it { should exist }
 end
